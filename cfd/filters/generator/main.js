5a0042b7a44d3cab52e358d8a6eef1fc1da5bb20
adolgov <adolgov@multicharts.com>
2017-04-03 17:22:09 +0300

    https://xbugs.tradingview.com/browse/TV-9546 set up sectors

--- cfd/filters/generator/main.js
+++ cfd/filters/generator/main.js
@@ -3,17 +3,17 @@ var requestSync = require("sync-request"),
 
 const dstPath = "../cfd.json";
 const groups =[
-    "http://hub1.tradingview.com:8094//symbols/dxy",
-    "http://hub1.tradingview.com:8094//symbols/us_chicago_indices",
-    "http://hub1.tradingview.com:8094//symbols/us_ny_indices",
-    "http://hub1.tradingview.com:8094//symbols/japan_indices",
-    "http://hub1.tradingview.com:8094//symbols/china_indices",
-    "http://hub1.tradingview.com:8094//symbols/european_indices",
-    "http://hub1.tradingview.com:8094//symbols/british_indices",
-    "http://hub1.tradingview.com:8094//symbols/spanish_indices",
-    "http://hub1.tradingview.com:8094//symbols/government_bonds",
-    "http://hub1.tradingview.com:8094//symbols/euro_bonds",
-    "http://hub1.tradingview.com:8094//symbols/forex_tvc",
+    "http://hub1.tradingview.com:8094/symbols/dxy",
+    "http://hub1.tradingview.com:8094/symbols/us_chicago_indices",
+    "http://hub1.tradingview.com:8094/symbols/us_ny_indices",
+    "http://hub1.tradingview.com:8094/symbols/japan_indices",
+    "http://hub1.tradingview.com:8094/symbols/china_indices",
+    "http://hub1.tradingview.com:8094/symbols/european_indices",
+    "http://hub1.tradingview.com:8094/symbols/british_indices",
+    "http://hub1.tradingview.com:8094/symbols/spanish_indices",
+    "http://hub1.tradingview.com:8094/symbols/government_bonds",
+    "http://hub1.tradingview.com:8094/symbols/euro_bonds",
+    "http://hub1.tradingview.com:8094/symbols/forex_tvc",
     "http://hub1.tradingview.com:8094/symbols/oanda",
     "http://hub1.tradingview.com:8094/symbols/forex_tvc"
 ];
@@ -36,23 +36,58 @@ groups.forEach(function(path){
     });
 });
 
+var bondsMarks = [
+    "TREASURY NOTE", "BOND"
+];
+
+var indexMarks = [
+    "INDEX", "NASDAQ", "RUSSELL", "S&P", "DOW JONES", "STOXX", "Australia", "Swiss", "Germany", "Europe", "France", "Hong Kong", "Japan"
+];
+
+var commoditiesMarks = [
+    "Brent", "WTI", "GOLD", "SILVER", "Corn", "Gas", "PALLADIUM", "PLATINUM"
+];
+
+function matches(s, values){
+    for (var i = 0; i < values.length; i++){
+        if (s.indexOf(values[i])>=0){
+            return true;
+        }
+    }
+    return false;
+}
+
 function tryDetectCategory(f){
-    if (f[1] === "index"){
+    if (f[1] === "index" || matches(f[5], indexMarks)){
         return "index";
     }
-    if (f[5].indexOf("BOND")>=0){
+    if (matches(f[5], bondsMarks)){
         return "bond";
+    }
+    if (matches(f[5], commoditiesMarks)){
+        return "commodity";
     }
     return ""; // TODO
 }
 
+var emptyCategoryCount = 0;
 var dstSymbols = [];
 symbols.forEach(function(s){
     var dst = {f:[]};
     dst.s = s.s;
-    dst.f[0] = tryDetectCategory(s.f);
+    var cat = tryDetectCategory(s.f);
+    if (!cat){
+        emptyCategoryCount++;
+    }
+    dst.f[0] = cat;
     dstSymbols.push(dst);
 });
+
+dstSymbols.sort(function(l,r){
+    return l.s.localeCompare(r.s);
+});
+
+console.info("Symbols with empty category is " + emptyCategoryCount);
 
 fs.writeFileSync(dstPath, JSON.stringify(
     {"time": new Date().toISOString()+'', "fields": ["sector"], "symbols": dstSymbols}, null, 2));

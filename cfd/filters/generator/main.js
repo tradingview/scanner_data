2ad3bb54741b21dbce0ee1731af5da297963b8ee
adolgov <adolgov@multicharts.com>
2017-04-25 18:40:52 +0300

    fix regions

--- cfd/filters/generator/main.js
+++ cfd/filters/generator/main.js
@@ -89,10 +89,10 @@ var regionMarks = {
   "Middle East": ["TURKEY"],
   "Asia": ["CHINA","HONG KONG", "Hong Kong","INDIA","INDONESIA","JAPAN","KOREA","MALAYSIA","SINGAPORE","Singapore","THAILAND"],
   "Europe": ["EURO CURRENCY INDEX","BRITISH POUND CURRENCY INDEX","SWISS FRANC CURRENCY INDEX","BELGIUM","FRANCE","GERMAN", "Germany","IRELAND","ITALY","NETHERLANDS", "Netherlands","NORWAY","PORTUGAL","SPAIN", "Swiss","UK"],
-  "Americas": ["NYSE","NASDAQ","S&P 500","US","THOMSON REUTERS","CANADIAN DOLLAR CURRENCY INDEX"],
+  "Americas": ["NYSE","NASDAQ","S&P 500","US ","THOMSON REUTERS","CANADIAN DOLLAR CURRENCY INDEX","US GOVERNMENT BONDS","DOW JONES","RUSSELL"],
   "Africa": ["SOUTH AFRICA"],
   "Pacific": ["AUSTRALIA", "Australia", "NEW ZEALAND DOLLAR CURRENCY INDEX"],
-  "":["CRUDE OIL", "Corn", "Natural Gas", "Soybeans", "Sugar", "Wheat", "Copper", "GOLD/SILVER RATIO"]
+  "":["CRUDE OIL", "Corn", "Natural Gas", "Soybeans", "Sugar", "Wheat", "Copper", "GOLD", "SILVER", "PLATINUM", "PALLADIUM"]
 };
 
 

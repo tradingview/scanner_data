47f3d3e7a662b9559cddf7489d08e53b60df61ce
adolgov <adolgov@multicharts.com>
2017-08-15 20:33:25 +0300

    https://xbugs.tradingview.com/browse/TV-10567 Источник данных для поля sector

---
+++ crypto/attributes/generator/main.js
@@ -0,0 +1,82 @@
+var requestSync = require("sync-request"),
+    fs = require("fs");
+
+const dstPath = "../crypto.json";
+
+var scanResp = requestSync("POST", "http://scanner.tradingview.com/crypto/scan2", {
+    json: {
+        sort: {
+            sortBy: "volume",
+            sortOrder: "desc"
+        },
+        filter: [
+            {
+                left: "volume",
+                operation: "nempty"
+            },
+            {
+                left: "name",
+                operation: "match",
+                right: "USD$|BTC$"
+            }]
+    }
+});
+if (scanResp.statusCode != 200) {
+    throw Error(scanResp.statusCode);
+}
+
+var coinMktCapResp = requestSync("GET", "https://api.coinmarketcap.com/v1/ticker/");
+if (coinMktCapResp.statusCode != 200) {
+    throw Error(coinMktCapResp.statusCode);
+}
+
+function getFirstCurrency(ticker) {
+    return ticker.substring(0, ticker.length - 3);
+}
+
+var tickers = {};
+var selectedSymbols = {};
+JSON.parse(scanResp.getBody()).symbols.forEach(function (s) {
+    var ticker = s.s.split(':')[1];
+    if (!tickers[ticker]) {
+        tickers[ticker] = ticker;
+        selectedSymbols[getFirstCurrency(ticker)] = s.s;
+    }
+});
+
+var currencyMapping = {
+    "BTU": "BCU",
+    "MIOTA": "IOT",
+    "XLM": "STR",
+    "USNBT": "NBT"
+};
+
+var dstSymbols = [];
+JSON.parse(coinMktCapResp.getBody()).forEach(function (s) {
+    var key = s.symbol;
+    var sym = selectedSymbols[key];
+    if (!sym) {
+        key = currencyMapping[s.symbol];
+        sym = selectedSymbols[key];
+    }
+    if (!sym) {
+        // console.warn("Skip " + s.symbol);
+    } else {
+        dstSymbols.push({
+            s: sym,
+            f: [s.name]
+        });
+
+        delete selectedSymbols[key];
+    }
+});
+
+for(var s in selectedSymbols){
+    console.warn("Symbol " + s + " not mapped!");
+}
+
+fs.writeFileSync(dstPath, JSON.stringify({
+    "time": new Date().toISOString() + '',
+    "fields": ["sector"],
+    "symbols": dstSymbols
+}, null, 2));

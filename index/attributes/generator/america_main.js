15e045001f8591c4ea64235f20b725d198817156
Andrey Mikhalchenko <50140694+livensmile@users.noreply.github.com>
2019-08-13 08:51:33 +0300

    Update america_main.js

--- index/attributes/generator/america_main.js
+++ index/attributes/generator/america_main.js
@@ -8,47 +8,47 @@ const groups = JSON.parse(fs.readFileSync("../../groups/list.json")).symbols.map
 const sectorNames = {};
 [
     {
-        "s": "SP:SPF",
+        "s": "SPCFD:SPF",
         "f": ["FINANCIALS"]
     },
     {
-        "s": "SP:SPN",
+        "s": "SPCFD:SPN",
         "f": ["ENERGY"]
     },
     {
-        "s": "SP:S5HLTH",
+        "s": "SPCFD:S5HLTH",
         "f": ["HEALTH CARE"]
     },
     {
-        "s": "SP:S5INFT",
+        "s": "SPCFD:S5INFT",
         "f": ["INFORMATION TECHNOLOGY"]
     },
     {
-        "s": "SP:S5CONS",
+        "s": "SPCFD:S5CONS",
         "f": ["CONSUMER STAPLES"]
     },
     {
-        "s": "SP:S5MATR",
+        "s": "SPCFD:S5MATR",
         "f": ["MATERIALS"]
     },
     {
-        "s": "SP:S5COND",
+        "s": "SPCFD:S5COND",
         "f": ["CONSUMER DISCRETIONARY"]
     },
     {
-        "s": "SP:S5UTIL",
+        "s": "SPCFD:S5UTIL",
         "f": ["UTILITIES"]
     },
     {
-        "s": "SP:S5INDU",
+        "s": "SPCFD:S5INDU",
         "f": ["INDUSTRIALS"]
     },
     {
-        "s": "SP:S5TELS",
+        "s": "SPCFD:S5TELS",
         "f": ["TELECOMMUNICATION SERVICES"]
     },
     {
-        "s": "SP:S5REAS",
+        "s": "SPCFD:S5REAS",
         "f": ["REAL ESTATE"]
     }
 ].forEach(s => sectorNames[s.s] = s.f[0]);
@@ -98,12 +98,12 @@ groups.forEach(function (path) {
 const symbolsPriorities = {};
 [].concat([
     // US Indices
-    "SP:SPX",
-    "SP:SVX",
-    "SP:MID",
-    "SP:OEX",
-    "SP:SPGSCI",
-    "DJ:DJI",
+    "SPCFD:SPX",
+    "SPCFD:SVX",
+    "SPCFD:MID",
+    "SPCFD:OEX",
+    "SPCFD:SPGSCI",
+    "DJCFD:DJI",
     "NASDAQ:IXIC",
     "NASDAQ:NDX",
     "RUSSELL:RUA",
@@ -119,17 +119,17 @@ const symbolsPriorities = {};
     "NASDAQ:UTY",
     "NASDAQ:SOX",
 ]).concat([
-    "SP:S5COND",
-    "SP:S5CONS",
-    "SP:S5HLTH",
-    "SP:S5INDU",
-    "SP:S5INFT",
-    "SP:S5MATR",
-    "SP:S5REAS",
-    "SP:S5TELS",
-    "SP:S5UTIL",
-    "SP:SPF",
-    "SP:SPN",
+    "SPCFD:S5COND",
+    "SPCFD:S5CONS",
+    "SPCFD:S5HLTH",
+    "SPCFD:S5INDU",
+    "SPCFD:S5INFT",
+    "SPCFD:S5MATR",
+    "SPCFD:S5REAS",
+    "SPCFD:S5TELS",
+    "SPCFD:S5UTIL",
+    "SPCFD:SPF",
+    "SPCFD:SPN",
 ]).forEach((s, i) => symbolsPriorities[s] = i);
 
 const forExclude = ["CBOE:OEX", "CBOE:RUI"];
